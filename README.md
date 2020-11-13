42353d745fd546eb9b41921ae481553a50b4100c
psk <pakker4321@itnomads.com>
2020-11-13 15:45:16 +0900

    JavaScript Library Draws an Excel Table

---
+++ README.md
@@ -0,0 +1,70 @@
+# Excel Table
+> excelTable.js는 엑셀 데이터 결과를 엑셀표 모양으로 출력하는 자바스크립트 라이브러리 
+> 원본 엑셀을 출력하고, 에러난 부분을 추가로 표시한다.
+
+## 사용 방법
+- 웹 페이지에서 사용하려면 hangul.js 파일을 <script>태그를 이용하여 추가
+- bootstrap 및 jquery 가 필요함
+	```	<link rel="stylesheet" href="./excelTable.css"/>
+		<script src="excelTable.js"></script>
+	``` 
+- 작성 방법
+  - target or targetObj
+    - 필수 값
+    - target은 id명을 입력
+    - targetObj는 객체를 입력
+  - visibleUnique
+    -  유니크 키 표시 유무
+  - data
+    - 배열로 전달 시
+      - 배열 형태로 전달 한다.
+    - 객체로 전달 시 
+	    - 필수 값
+	    - header
+	      - 엑셀 상위의 컬럼명
+	      - displayName 출력되는 컬럼 명
+	      - columnName 값이 origin의 key값과 연결되어 출력
+	      - dataType NUMBER or STRING 에 따라 왼쪽, 오른쪽 정렬 됨
+	      - unique 상위의 visibleUnique가 true일 때 컬럼 왼편에 키 표기
+	    - origin
+	      - 원본 데이터
+	    - errors
+	      - 에러난 컬럼 명시
+	      - 에러난 컬럼이 존재하는 행과 열의 폰트 색을 바꿔줌
+  - style
+    - 미작성시 default 스타일로 지정(생략가능)
+    - fontColor, fontSize, backgroundColor, warnColor(error) 커스텀 가능
+```
+	excelTable.init({
+		//target : "targetTable", 						
+		targetObj : $("#targetTable").get(0),
+		visibleUnique : false, 				
+		//data : [
+		//	["네이버","7","그룹1","그룹2","그룹3"], 
+		//	["다음","10","그룹1","그룹2","그룹3","도메인"], 
+		//	["구글","15","그룹1","그룹2","그룹3","도메인"], 
+		//],
+		data : {
+			header : header,
+			origin : origin,
+			errors : errors,
+		},
+		style : {
+			edge : {
+				fontColor : 'green', fontSize : '20px'
+			},
+			header : {
+				backgroundColor  : 'green', fontColor : 'white'
+			},
+			cell : {
+				backgroundColor  : 'blue' , fontColor : 'yellow'
+			},
+			error : {
+				warnColor : 'grey', fontColor : 'white'
+			}
+		}		
+	});
+```
+
+## 예시
+- index.html 참고
---
+++ excelTable/excelTable.js
@@ -0,0 +1,297 @@
+(function () {
+    //ie foreach
+    if (window.NodeList && !NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
+
+    let excelTable = {
+
+        init: function (obj) {
+
+            if (!this.validation(obj)) {
+                console.error('알맞은 데이터를 입력해 주세요.');
+                return;
+            }
+
+            this.object = $.extend(true, this.getDefaultStructure(obj), obj);
+            this.target = this.targetInit();
+            let thead = this.target.querySelector("thead");
+            let tbody = this.target.querySelector("tbody");
+
+            if (Array.isArray(obj.data)) { 
+                 this.simpleInitSheet(thead, tbody);
+
+            } else {
+                this.setHeader(thead);
+	            this.initSheet(thead, tbody);
+	            if (this.object.data.errors && this.object.data.errors.length > 0) {
+	                this.setError(tbody);
+	            }
+	            
+            }
+
+        },
+
+        validation: function (obj) {
+            if (!obj.target && !obj.targetObj) {
+                console.error('target 또는 targetObj 는 필수입니다.');
+                return;
+            }
+
+            if (!obj.data) {
+            	console.error('data는 필수입니다.');
+                return;
+            }
+
+            if (!Array.isArray(obj.data)) {
+                if (!obj.data.header) {
+                    console.error('header는 필수입니다.');
+                    return;
+                }
+
+                if (!obj.data.origin) {
+                    console.error('origin은 필수 입니다.');
+                    return;
+                }
+
+            }
+
+            if (obj.hasOwnProperty('targetObj') && typeof obj.targetObj === "array") {
+        	    console.error("targetObj는 단일대상만 지원합니다.");
+                return;
+            }
+
+            return true;
+
+        },
+
+        getDefaultStructure : function(obj) {
+        	let data = obj.data;
+        	if (!Array.isArray(obj.data)) {
+        		data = {
+                    header: obj.data.header,
+                    origin: obj.data.origin,
+                    errors: obj.data.errors,
+                }
+        	} 
+
+        	return {
+                target: obj.target,
+                targetObj: null,
+                visibleUnique: false,
+                data: data,
+                style: {
+	                edge: {
+	                    fontColor: '#6c6b70', backgroundColor: '#a9a9a9', fontSize: '13px'
+	                },
+	                header: {
+	                    fontColor: '#6c6b70', backgroundColor: '#d5d5d5', fontSize: '13px'
+	                },
+	                cell: {
+	                    fontColor: "#6c6b70", backgroundColor: null, fontSize: '13px'
+	                },
+	                error: {
+	                    warnColor: 'yellow', fontColor: "white", backgroundColor: 'rgb(226, 92, 77)', fontSize: '13px'
+	                }
+	            }
+            };
+        },
+
+        targetInit: function () {
+            this.target = (this.object.targetObj == null) ? document.getElementById(this.object.target) : this.object.targetObj;
+            
+            let that = this;
+            let ary = ["thead", "tbody"];
+            ary.forEach(function (element) {
+                let tag = that.target.querySelector(element);
+                if (tag) {
+                    tag.innerHTML = '';
+                } else {
+                    that.target.appendChild(document.createElement(element));
+                }
+            });
+
+            return this.target;
+
+        },
+
+        makeAlphabet: function (objlength) {
+            let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            let alphabetAry = (' ' + alphabet).split('');
+
+            let num = objlength - alphabet.length; //만들어야 할 알파벳 갯수
+
+            if (num <= 0) {
+                return alphabetAry;
+            }
+
+            let all = parseInt(num / alphabet.length); //전체 돌릴수
+            let remainder = num % alphabet.length; // 나머지
+
+            for (let i = 0; i <= all; i++) {
+                let cnt = (i === all) ? remainder : alphabet.length;
+
+                for (let j = 0; j < cnt; j++) {
+                    alphabetAry.push(alphabet.charAt(i) + alphabet.charAt(j));
+                }
+            }
+            return alphabetAry;
+
+        },
+
+        setEdge: function (excelThead, objlength) {
+            let that = this;
+            let alphabetAry = that.makeAlphabet(objlength);
+            let excelColumn = excelThead.insertRow();
+
+            for (let i = 0; i <= objlength; i++) {
+                let excelTd = excelColumn.insertCell();
+                if (i == 0) {
+                    excelTd.style.width = '5%';
+                }
+                that.settings(excelTd, 'ev-column', alphabetAry[i]);
+                that.designStyle(excelTd, this.object.style.edge);
+
+            }
+        },
+
+        setNumRow: function (tr, index) {
+            let excelHeaderTd = tr.insertCell();
+
+            this.settings(excelHeaderTd, 'ev-row', index)
+            this.designStyle(excelHeaderTd, this.object.style.edge);
+
+        },
+
+        setHeader: function (excelThead) {
+            let that = this;
+            that.setEdge(excelThead, this.object.data.header.length);
+
+            //edge 열 번호
+            let excelHeader = excelThead.insertRow();
+            that.setNumRow(excelHeader, 1);
+
+            this.object.data.header.forEach(function (element) {
+                let excelTd = excelHeader.insertCell();
+
+                let innerHtml = (that.object.visibleUnique && element.unique) ? '<span class="key"></span>' + element.displayName : element.displayName;
+                that.settings(excelTd, 'ev-displayName', innerHtml)
+                that.designStyle(excelTd, that.object.style.header);
+
+                excelTd.dataset.columnName = element.columnName;
+                excelTd.dataset.dataType = element.dataType;
+
+            });
+
+        },
+
+        initSheet: function (thead, excelBody) {
+            let that = this;
+
+            this.object.data.origin.forEach(function (rowData, idx) {
+                let excelColumn = excelBody.insertRow();
+                that.setNumRow(excelColumn, idx + 2);
+
+                // 인스턴스 추가
+                let selectorColumns = thead.querySelectorAll("tr")[1].querySelectorAll("td:not(.ev-row)");
+
+                selectorColumns.forEach(function (coloumsData) {
+                    let excelTd = excelColumn.insertCell();
+
+                    let className = (coloumsData.dataset.dataType.toUpperCase() === 'NUMBER') ? 'text-right ellipsis ev-cell' : 'text-left ellipsis ev-cell';
+                    that.settings(excelTd, className, rowData[coloumsData.dataset.columnName])
+                    that.designStyle(excelTd, that.object.style.cell);
+
+                });
+
+            });
+        },
+
+        setError: function (body) {
+            let that = this;
+            this.object.data.errors.forEach(function (element) {
+                let errorData = that.findTd(body, element.row, element.column.toUpperCase().charCodeAt());
+
+                let icon = '<span class="glyphicon glyphicon-warning-sign" style="color:yellow;">&nbsp;</span>';
+                if (that.isTextAlignRight(errorData)) {
+                    icon = '<span class="glyphicon glyphicon-warning-sign" style="padding-right:70%;color:yellow;">&nbsp;</span>';
+                }
+
+                that.settings(errorData, 'ev-error', icon + errorData.innerText.trim(), true);
+                that.designStyle(errorData, that.object.style.error, true);
+
+                errorData.title = element.errorMessage;
+                that.setErrorEdge(body, element.row, element.column.toUpperCase().charCodeAt());
+
+            });
+        },
+
+        simpleInitSheet: function (thead, excelBody) {
+            let that = this;
+
+            let headerLength = this.object.data.reduce(function (maxLength, element, index, array) {
+                if (index == that.object.data.length - 1) {
+                    let maxLengthIdx = element.length > array[maxLength].length ? index : maxLength
+                    return array[maxLengthIdx].length;
+                }
+                return element.length > array[maxLength].length ? index : maxLength;
+            }, 0);
+
+            this.setEdge(thead, headerLength);
+            this.object.data.forEach(function (rowData, idx) {
+                let excelColumn = excelBody.insertRow();
+                that.setNumRow(excelColumn, idx + 1);
+
+                // 인스턴스 추가
+                for (let i = 0; i < headerLength; i++) {
+                    let excelTd = excelColumn.insertCell();
+                    let className = 'text-left ellipsis ev-cell';
+                    let innerHtml = rowData[i] === undefined ? '' : rowData[i];
+
+                    that.settings(excelTd, className, innerHtml);
+                    that.designStyle(excelTd, that.object.style.cell);
+                }
+
+            });
+        },
+
+        settings: function (excelTd, className, innerHtml, isError) {
+            excelTd.innerHTML = innerHtml;
+
+            if (isError) {
+                excelTd.classList.add(className);
+            } else {
+                excelTd.className = className;
+            }
+
+        },
+
+        designStyle: function (target, styleObj, isError) {
+            target.style.color = styleObj.fontColor;
+            target.style.background = styleObj.backgroundColor;
+            target.style.fontSize = styleObj.fontSize;
+
+            if (isError) {
+                target.querySelector('span').style.color = styleObj.warnColor;
+            }
+
+        },
+
+        findTd: function (body, row, colunmAsciiCode) {
+            let column = colunmAsciiCode - 64; // A의 ascii code 값을 빼서 몇번째 알파벳인지 알아냄
+            return body.querySelectorAll("tr")[row - 2].querySelectorAll("td")[column];
+        },
+
+        setErrorEdge: function (body, row, colunmAsciiCode) {
+            let column = colunmAsciiCode - 64; // A의 ascii code 값을 빼서 몇번째 알파벳인지 알아냄
+            this.target.querySelector("thead tr").querySelectorAll('td')[column].style.color = this.object.style.error.backgroundColor;
+            body.querySelectorAll("tr")[row - 2].querySelector(".ev-row").style.color = this.object.style.error.backgroundColor;
+        },
+
+        isTextAlignRight: function (errorData) {
+            return errorData.getAttribute('class').indexOf('text-right') != -1 ? true : false;
+        }
+    }
+
+    window.excelTable = excelTable;
+})();
