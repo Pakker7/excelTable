7db632e31b09efd8173b33ebeec4d4851baef3bd
psk <pakker4321@itnomads.com>
2021-02-01 17:32:13 +0900

    전송되는 데이터의 변경으로 인한 수정(excelData -> 서브시트를 포함한 모든 원본 데이터가 전달됨) 추가 수정
    - 서브시트 출력 크기를 늘릴 것
    - 계정 명, 그룹등록 이 헤더도 빨간색으로 칠할 것
    - 노란삼각형에 커서 대면 메세지 나오게 바꿀 것

--- excelTable/excelTable.js
+++ excelTable/excelTable.js
@@ -20,7 +20,7 @@
             let tbody = this.target.querySelector("tbody");
 
             if (Array.isArray(obj.data)) { // 배열 형태로 input 될 경우 에러 표시가 없는 간단한 형태의 엑셀이 출력된다.
-                this.simpleInitSheet(thead, tbody, this.object.data); //TODO simpleSetHeader가 추가되서 수정이 필요함
+                this.simpleInitSheet(thead, tbody, this.object.data);
 
             } else {
 
@@ -38,29 +38,36 @@
                 let subSheetThead = subSheetTarget.querySelector("thead");
                 let subSheetTbody = subSheetTarget.querySelector("tbody");
 
-                this.simpleSetHeader(subSheetThead, subSheetObject.data.headerAry, subSheetObject.data.origin);
-                this.simpleInitSheet(subSheetThead, subSheetTbody, subSheetObject.data.origin);
+                // 서브시트 1개 기준으로 짜고, 이거 자체를 for문돌려서 추가하는 식으로 서브시트 여러개일 때 적용 예정
+                let subSheetHtmlAry = [];
+                for (var i = 1; i < this.object.data.excelData.length ; i++) {
+
+                    this.simpleSetHeader(subSheetThead, subSheetObject.data.headerAry, this.object.data.excelData[i]); // TODO 서브시트 여러개일 때 index넣어주는 걸로 변경
+                    this.simpleInitSheet(subSheetThead, subSheetTbody, this.object.data.excelData[i]);
+                    if (subSheetObject.data.errors && subSheetObject.data.errors.length > 0) {
+                        this.setError(subSheetTarget, subSheetTbody, false);
+                        this.subSheetSetErrorEdge(tbody, this.categorizationErros(false));
+                    }
+                    subSheetHtmlAry.push($('#' + this.subSheetTmpId).html())
+                }
+
+                //let subSheetHtml = $('#' + this.subSheetTmpId).html();
+                //this.connectSubSheetToMainSheet(subSheetObject, subSheetHtml, tbody);
+                this.connectSubSheetToMainSheet(subSheetObject, subSheetHtmlAry, tbody);
                 
-                let subSheetErrors = this.categorizationErros(false);
-                this.subSheetSetError(subSheetObject, subSheetErrors);
-                this.settingSubSheetError(tbody, subSheetErrors);
-
-                let subSheetHtml = $('#' + this.subSheetTmpId).html();
-                this.connectSubSheetToMainSheet(subSheetObject, subSheetHtml, tbody);
-
             }
         },
 
         subSheetSetError : function (subSheetObject, subSheetErrors) {
             let that = this;
             subSheetErrors.forEach(function (errorMsg, index) {
-                //1. 시트 별 데이터에서 실제 데이터 값(계정명, 본인 값)을 얻어온다
+                // 1. 시트 별 데이터에서 실제 데이터 값(계정명, 본인 값)을 얻어온다
                 let errorOriginData = that.getOriginData(subSheetObject, errorMsg);
 
-                // //2. 1번 데이터를 사용하여 그림 그린 서브시트의 실제 위치를 찾는다
+                // 2. 1번 데이터를 사용하여 그림 그린 서브시트의 실제 위치를 찾는다
                 let location = that.getSubSheetLocation(errorOriginData);
 
-                // // 3. 에러 메세지의 row, column을 임의로 그린 서브시트로 바꾼다.
+                // 3. 에러 메세지의 row, column을 임의로 그린 서브시트로 바꾼다.
                 errorMsg.column = location.column;
                 errorMsg.row = location.row;
             });
@@ -124,8 +131,7 @@
         },
 
         // 서브 시트에 에러가 있으면 해당하는 주시트의 edge와 td 색을 추가적으로 변경하는 함수
-        // TODO 서브시트 에러 표기하는 함수랑 합치던가....? 구조를 다시 짜던가..  리펙토링하기
-        settingSubSheetError : function(body, subSheetErrors) {
+        subSheetSetErrorEdge : function(body, subSheetErrors) {
 
             let that = this;
             // 1. 서브시트 에러의 column과 row를 구해서 object에 추가한다.
@@ -156,7 +162,6 @@
                 });
             });
 
-
             // row 구하기
             let kindOfSubSheetSet = new Set(); // 서브 시트 종류 구하기
             errorArrayByRow.forEach(function (arrayElement) {
@@ -171,9 +176,6 @@
                         data += element.data;
                         return (array.length -1 === index) ? data : data + ',';
                     },'');
-
-                    console.log('--------------------')
-                    console.log(subSheetDataByRow)
 
                     that.object.data.origin.some(function (element, index) {
 
@@ -194,11 +196,11 @@
             });
         },
 
-        connectSubSheetToMainSheet : function(subSheetObject, subSheetHtml, mainSheetTbody) {
+        connectSubSheetToMainSheet : function(subSheetObject, subSheetHtmlAry, mainSheetTbody) {
             let that = this;
 
             let subSheetThead =  document.querySelector('#' + this.subSheetTmpId + ' thead');
-            $('#' + this.subSheetTmpId + ' tbody tr').each (function (subSheetIndex, subSheetElement) {
+            $('#' + this.subSheetTmpId + ' tbody tr').each (function (subSheetIndex, subSheetElement) { 
                 
                 let subColumnkey = $(subSheetElement).find('.ev-cell:eq(0)').text();
 
@@ -210,7 +212,6 @@
                         return false;
                     }
                 });
-
 
                 $(mainSheetTbody).find('tr').each (function (mainSheetIndex, mainSheetTrElement) {
                     let mainColumnKeyTd = mainSheetTrElement.querySelectorAll("td")[mainSheetColumn];
@@ -230,12 +231,11 @@
             });
             this.popoverSetting(mainSheetTbody);
             $('#' + this.subSheetTmpId).remove();
-
         },
 
         setSubSheetStructure : function() {
             // 서브 시트도 주 시트처럼 엑셀로 다른 곳에 만들었다가 .html 로 코드를 그대로 옮기는 방식
-            $('body').append('<table id=subSheetDefaultStructure style=display:none><thead></thead><tbody></tbody></table>');
+            $('body').append('<table id=subSheetDefaultStructure><thead></thead><tbody></tbody></table>');
             return document.getElementById(this.subSheetTmpId);
         },
 
@@ -552,36 +552,42 @@
 
             errors.forEach(function (element) {
                 let errorData = that.findTd(body, element.row, element.column);
-
-                //TODO tooltip에서 glyphicon-warning-sign에만 hover시에 tooltip이 나오는 걸로 수정?
-                let errorIcon = '<span class="glyphicon glyphicon-warning-sign" style="color:yellow;">&nbsp;</span>';
+                let errorIcon = '<div class=warn-sign><span class="glyphicon glyphicon-warning-sign" style="color:yellow;">&nbsp;</span></div>';
                 if (that.isTextAlignRight(errorData)) {
-                    errorIcon = '<div class="ev-warning"><span class="glyphicon glyphicon-warning-sign" style="color:yellow;">&nbsp;</span></div>';
+                    errorIcon = '<div class="ev-warning"><div class=warn-sign><span class="glyphicon glyphicon-warning-sign" style="color:yellow;">&nbsp;</span></div></div>';
                 }
 
                 that.settings(errorData, 'ev-error', errorIcon + errorData.innerText.trim(), true);
                 that.designStyle(errorData, that.object.style.error, true);
 
-                errorData.innerHTML = errorData.innerHTML + '<span class="ev-tooltip">' + element.errorMessage + '</span>';
+                errorData.querySelector('.warn-sign').innerHTML += '<span class="ev-tooltip">' + element.errorMessage + '</span>'; 
                 that.setErrorEdge(target, body, element.row, element.column);
             });
         },
-
 
         // 주 시트는 마지막 컬럼이 레이아웃을 넘어가지 않게 처리함
         // 서브 시트는 모든 컬럼이 레이아웃을 넘어가지 않게 처리함
         setErrorTooltipTdLast: function(body, isNomalErros) {
             $(body).find('tr').each(function (index, item) {
+
                 if (isNomalErros) {
                     let replaceClass = $(item).find('td:last').html().replace('ev-tooltip', 'ev-tooltip-side');
+                    let width = (Number($(item).find('td:last').css('width').replace('px','')) * 0.8) + 'px';
                     $(item).find('td:last').html(replaceClass);
-
+                    $(item).find('td:last .ev-tooltip-side').css('width',width);
+                
                 } else {
 
                     $(item).each (function (index, element) {
-                        $(element).find('td').each (function (tdIndex, tdElement ){
-                            let replaceClass = $(tdElement).html().replace('ev-tooltip', 'ev-tooltip-side')
-                            $(tdElement).html(replaceClass)
+                        let tdArray = $(element).find('td');
+                        tdArray.each (function (tdIndex, tdElement) {
+                            let multipleValue = 2.8;
+                            if (tdIndex == tdArray.length - 1) {
+                                multipleValue = 1.5
+                            } 
+                            let width = (Number($(tdElement).css('width').replace('px','')) * multipleValue) + 'px';
+                            $(tdElement).find('.ev-tooltip').css('width', width);
+                            
                         });
                     });
                 }
@@ -589,17 +595,18 @@
             });
         },
 
-        simpleInitSheet: function (thead, excelBody, objectData) {
+        simpleInitSheet: function (thead, excelBody, excelData) {
             let that = this;
-            objectData.forEach(function (rowData, idx) {
+
+            excelData.origin.forEach(function (rowData, idx) {
                 let excelColumn = excelBody.insertRow();
                 that.setNumRow(excelColumn, idx + 2);
 
-                let length = that.simpleGetLength(objectData); 
+                let length = that.simpleGetLength(excelData);
                 for (let i = 0; i < length; i++) {
                     let excelTd = excelColumn.insertCell();
                     let className = 'text-left ev-ellipsis ev-cell';
-                    let innerHtml = rowData[i] === undefined ? '' : rowData[i];
+                    let innerHtml = rowData[that.getColumnAlphabet(i+1)] === undefined ? '' : rowData[that.getColumnAlphabet(i+1)];
 
                     that.settings(excelTd, className, innerHtml);
                     that.designStyle(excelTd, that.object.style.cell);
@@ -608,9 +615,10 @@
             });
         },
 
-        simpleSetHeader : function (thead, header, objectData) {
+        simpleSetHeader : function (thead, header, excelData) {
             let that = this;        
-            let length = this.simpleGetLength(objectData); // origin 데이터의 사이즈에 따라 총 길이가 정해진다
+            
+            let length = this.simpleGetLength(excelData)
             this.setEdge(thead, length);
 
             let excelColumn = thead.insertRow();
@@ -624,17 +632,10 @@
                 that.settings(excelTd, className, innerHtml);
                 that.designStyle(excelTd, that.object.style.cell);
             }
-
         },
 
-        simpleGetLength : function (objectData) {
-            return objectData.reduce(function (maxLength, element, index, array) {
-                if (index == objectData.length - 1) {
-                    let maxLengthIdx = element.length > array[maxLength].length ? index : maxLength
-                    return array[maxLengthIdx].length;
-                }
-                return element.length > array[maxLength].length ? index : maxLength;
-            }, 0);
+        simpleGetLength : function (excelData) {
+            return Object.keys(excelData.origin[0]).length // excelData는 길이가 다 같게 넘어옴
         },
 
         settings: function (excelTd, className, innerHtml, isError) {
@@ -653,8 +654,8 @@
             target.style.background = styleObj.backgroundColor;
             target.style.fontSize = styleObj.fontSize;
 
-            if (isError && target.querySelector('span') != undefined) {
-                target.querySelector('span').style.color = styleObj.warnColor;
+            if (isError && target.querySelector('.warn-sign') != undefined) {
+                target.querySelector('.warn-sign').querySelector('span').style.color = styleObj.warnColor;
             }
 
         },
@@ -669,7 +670,8 @@
         setErrorEdge: function (target, body, row, column) { 
             let index = 2;
             let columnResult = this.getColumnIndex(column);
-
+            target.querySelectorAll("thead tr")[1].querySelectorAll('td')[columnResult].style.backgroundColor = this.object.style.error.backgroundColor;
+            target.querySelectorAll("thead tr")[1].querySelector('td').style.backgroundColor = this.object.style.error.backgroundColor;
             target.querySelector("thead tr").querySelectorAll('td')[columnResult].style.backgroundColor = this.object.style.error.backgroundColor;
             body.querySelectorAll("tr")[row - index].querySelector(".ev-row").style.backgroundColor = this.object.style.error.backgroundColor;
         },
